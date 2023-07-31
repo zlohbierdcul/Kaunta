a0e75636acfe1d89dc259a93bdd2b4bdaf85bb83
zlohbierdcul <luc.dreibholz@gmx.net>
2023-08-01 01:54:04 +0200

    changes

--- src/main/controller/search_input_controller.py
+++ src/main/controller/search_input_controller.py
@@ -6,7 +6,7 @@ from engine.search_engine import find_show, find_season, find_episodes
 from discord.ui import View
 
 async def handle_search(bot, interaction: discord.Interaction, input: str):
-    interaction.response.defer()
+    await interaction.response.defer()
     results = find_show(input)
     if (len(results) > 0):
         for index,x in enumerate(results):
@@ -15,11 +15,11 @@ async def handle_search(bot, interaction: discord.Interaction, input: str):
         select_menu = ShowSelectMenu("Your results..", 1, select_options, False)
         abort_button = AbortButton()
         embeded = discord.Embed(title="Results", description=f"Showing only the {len(select_options)} first results!\nPlease select one!", color=discord.Color.from_rgb(0,255,0))
-        view = View(None)
+        view = View(timeout=None)
         view.add_item(select_menu)
         view.add_item(abort_button)
-        await interaction.response.send_message(embed=embeded, view=view)
+        await interaction.followup.send(embed=embeded, view=view)
     else:
-        await interaction.response.send_message(embed=discord.Embed(title="Noting", description="I found absolutely noting. \nPlease try again!", color=discord.Color.from_rgb(255,0,0)), delete_after=5)
+        await interaction.followup.send(embed=discord.Embed(title="Noting", description="I found absolutely noting. \nPlease try again!", color=discord.Color.from_rgb(255,0,0)), delete_after=5)
     
     
